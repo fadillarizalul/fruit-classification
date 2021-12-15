dae98219f0f5bd6a1f9dc52946f8e07c4434dda1
Fadilla Rizalul <76253393+fadillarizalul@users.noreply.github.com>
2021-12-15 10:15:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model = tf.keras.models.load_model('model1.h5')
+  model = tf.keras.models.load_model('model.h5')
   return model
   
 with st.spinner('Model is being loaded..'):
